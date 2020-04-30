ca3509bd852ac38f377ca5780aeebce157480cfb
tryagain4 <mastercode01@outlook.com>
2020-04-29 23:55:19 -0700

    add RandomWalk

---
+++ RandomWalk.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 20:25:45 2020
+
+@author: Xiaocheng
+"""
+import random
+import networkx as nx
+import numpy as np
+
+class RandomWalk:
+    def __init__(self, adj, ):
+        pass
+    def walk():
